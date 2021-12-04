031c95d743734cda9741c3bb8045a50c87f378fd
Karelo2705 <karlo.blaskovic@gmail.com>
2021-12-05 00:09:40 +0100

    Added user data input

---
+++ Domain/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    internal class User
+    {
+        public String name { get; set; }
+        public String surname { get; set; }
+
+        public String adress { get; set; }
+
+        public int Distance { get; set; }
+
+
+        public User(String Name, String Surname, String Adress)
+        {
+            name = Name;
+            surname = Surname;
+            adress = Adress;
+            Distance = DistanceGen();
+        }
+        public int DistanceGen()
+        {
+            var num = new Random();
+            return (num.Next(50, 999));
+        }
+    }
+}
---
+++ NiceApp/ForPrettierCode.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using Domain;
+
+
+namespace NiceApp
+{
+    public class ForPrettierCode
+    {
+        public static string Menu()
+        {
+            Console.WriteLine("||||||||||||||||||||");
+            Console.WriteLine("Little hardware shop");
+            Console.WriteLine("||||||||||||||||||||\n");
+            Delay();
+            Console.WriteLine("Za pristup sustavu prvo se trebate prijaviti.");
+            Delay();
+            Console.WriteLine("Unesite svoje ime (samo ime, bez prezimena): ");
+            var name = Console.ReadLine();
+            Console.WriteLine("Unesite svoje prezime: ");
+            var surname = Console.ReadLine();
+            Console.WriteLine("Unesite adresu ulice u koju želite da paket bude dostavljen: ");
+            var adress = Console.ReadLine();
+
+
+            var newUser = new User(name, surname, adress);
+
+
+        }
+        private static void Delay()
+        {
+            Task.Delay(2000).Wait();
+        }
+    }
+}
